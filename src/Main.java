e106cf991d8a31cec46fed7b44a04b9f65d86d90
Merin <merint2000@gmail.com>
2024-04-12 12:28:13 +0530

    commit changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.lang.System;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+//        System.out.println("hello,world");
     }
 }
